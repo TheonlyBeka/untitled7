7913f7f8937614bb1843d11f0ce55ca72bfbec47
Бека <theonly.bekzat@gmail.com>
2021-08-26 18:26:49 +0600

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,32 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        int age = 18;
-        int temp = 15;
-        String rsult = getResult(age, temp);
-        System.out.println(rsult);
-        getResult(18, 26);
-        getResult(24, 15);
-        getResult(52, -2);
-        getResult(39,-12);
-        getResult(12, 0);
-    }
 
-    public static String getResult(int age, int temp) {
-        if (age >= 20 && age < 45 && temp >= -20 && temp <30) {
-            return "Можно идти гулять !";
-        }else if (age > 0 && age < 20 && temp >= 0 && temp <28){
-            return "Можно идти гулять !";
 
-        }else if (age > 45 && temp >= -10 && temp <25){
-            return "Можно идти гулять !";
+        class Cat {
+            public static void num() {
+                double[] array = {1.5, -2.4, 1.2, 5.5, -5.1, 1.8, 6.3, -8, 7, -5, 6, 15.2, 12.12, 20., 7.7, 8.6, 18.8};
+                int counter = 0;
+                int sum = 0;
+                boolean firstNegitiveNum = false;
 
-        }else {
-            return "Нельзя идти гулять !";
+                for (double num : array) {
+                    if (num < 0) {
+                        firstNegitiveNum = true;
+                    }
 
+                    if (firstNegitiveNum) {
+                        if (num > 0) {
+                            counter++;
+                            sum += num;
+                        }
+                    }
+                }
+                System.out.println("Сумма пол чис " + sum);
+                System.out.println("Среднее ариф " + sum / counter);
+
+            }
         }
 
     }
